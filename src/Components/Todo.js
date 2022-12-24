7ec37a34cc268c0a2d7f303629f2a78f06c2f6dd
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2022-12-23 18:19:05 -0600

    Work on Adding Events Per Day

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import LabelWInput from "./BaseComponents/LabelWInput";
+import { useNavigate } from "react-router-dom";
 
 export default function Todo() {
+  const navigate = useNavigate();
   const [adjustDate, setAdjustDate] = useState(0);
 
   var month = [
@@ -27,6 +31,18 @@ export default function Todo() {
     "Friday",
     "Saturday",
   ];
+
+  // function formatDate(date) {
+  //   var d = new Date(date),
+  //     month = "" + (d.getMonth() + 1),
+  //     day = "" + d.getDate(),
+  //     year = d.getFullYear();
+
+  //   if (month.length < 2) month = "0" + month;
+  //   if (day.length < 2) day = "0" + day;
+
+  //   return [year, month, day].join("-");
+  // }
 
   const today = new Date();
 
@@ -55,26 +71,57 @@ export default function Todo() {
     setAdjustDate(0);
   }
 
-  return (
-    <div className="FlexRowCenterCenter">
-      <div className="CalDisplay FlexColCenterCenter">
-        <div className="MonthText">{month[today.getMonth()]}</div>
-        <button onClick={handleYesterday}>Yesterday</button>
-        <button onClick={handleTomorrow}>Tomorrow</button>
-        <button onClick={handleCurrent}>Current</button>
+  const NewEvent = ["Task", "Comments", "Email"];
+  let data = { "ID": uuid() };
 
-        <div className="CalBoxDiv FlexRowCenterCenter">
-          {DateArray.map((item, index) => {
-            return (
-              <div className="CalBox">
-                <div className="Header">
-                  <div className="DayNumber">{item.getDate()}</div>
-                  <div className="DayText">{days[item.getDay()]}</div>
+  function AddTask(e) {
+    e.preventDefault();
+
+    NewEvent.forEach((element) => {
+      data[element] = document.getElementById(element).value;
+      document.getElementById(element).value = "";
+    });
+    data["Date"] = document.getElementById("Date").value;
+    navigate(0);
+  }
+
+  let PlaceHolder = {};
+  NewEvent.forEach((element) => {
+    PlaceHolder[element] = "";
+  });
+
+  return (
+    <div className="FlexColCenterCenter">
+      <div className="FlexRowCenterCenter">
+        <div className="CalDisplay FlexColCenterCenter">
+          <div className="MonthText">{month[today.getMonth()]}</div>
+          <button onClick={handleYesterday}>Yesterday</button>
+          <button onClick={handleTomorrow}>Tomorrow</button>
+          <button onClick={handleCurrent}>Current</button>
+
+          <div className="CalBoxDiv FlexRowCenterCenter">
+            {DateArray.map((item, index) => {
+              return (
+                <div className="CalBox" key={index}>
+                  <div className="Header">
+                    <div className="DayNumber">{item.getDate()}</div>
+                    <div className="DayText">{days[item.getDay()]}</div>
+                    {/* map over "event" localStorage Save, if formattedDate(item.toISOString) === event.Date return jsx Element */}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
+      </div>
+      <div className="NewTaskFormDiv">
+        <form onSubmit={AddTask}>
+          {NewEvent.map((item, index) => {
+            return <LabelWInput command={item} dataInput={""} key={index} />;
+          })}{" "}
+          <input type={"date"} id="Date" />
+          <input type={"submit"} />
+        </form>
       </div>
     </div>
   );
